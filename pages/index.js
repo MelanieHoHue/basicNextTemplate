4dec3f2cbb976afe1c468ae9afdc189bd133e266
M.H-H <Melanie.Holzapfel-Huettenrauch@Student.HTW-Berlin.de>
2021-05-13 13:06:00 +0200

    using getStaticProps for pre-rendering at build time

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,43 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
+      
       <Head>
         <title>{siteTitle}</title>
       </Head>
+      
       <section className={utilStyles.headingMd}>
         <h3>Basic Next.js Example</h3>
         <p>This template shall be the foundation for the thesis work I will programm</p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br/>
+              {id}
+              <br/>
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
